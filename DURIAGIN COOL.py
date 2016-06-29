4c86b292e815f8b9aceba8edc430ca703ebbab01
whitenights <whitenights@users.noreply.github.com>
2016-06-29 13:35:29 +0300

    Add files via upload

---
+++ DURIAGIN COOL.py
@@ -0,0 +1,23 @@
+import re
+def gettext():
+    f = open('text.txt', 'r', encoding = 'utf-8')
+    text = f.read()
+    f.close()
+    return text
+
+def getinitnames(text):
+    result = re.findall(r'([А-Я]\.\s[А-Я][^\s|\W|\d]+)|([А-Я][^\s|\W|\d]\.\s[А-Я][^\s|\W|\d]+)', text)
+    for el in result:
+        print(el)
+
+def getfullnames(text):
+    result = re.findall(r'([А-Я][^\s|\W|\d]\\s[А-Я][^\s|\W|\d]+)', text)
+    for el1 in result:
+        print(el1)
+
+def main():
+    num = gettext()
+    getinitnames(num)
+    getfullnames(num)
+    
+main()
